5230ad0eeb15a26d79da040d103a669c70057c25
Vpolyushkevich@gmail.com <Vasily>
2023-04-03 20:28:54 +0300

    Create client server

---
+++ Server/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Server
+{
+
+              class Program
+              {
+                            static void Main(string[]args)
+                            {
+                                          Console.WriteLine("Это наш сервер");
+                                          OurServer server = new OurServer();
+                                          
+                            }
+              }
+}
